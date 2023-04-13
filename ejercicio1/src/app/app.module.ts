dad4a8821fc1957b658c80f89de186d63f20c87c
pablo4192 <estanguet570@gmail.com>
2023-04-13 18:50:47 -0300

    commit ejercicio2

--- ejercicio1/src/app/app.module.ts
+++ ejercicio1/src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BienvenidoComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
---
+++ ejercicio1/src/app/usuario.ts
@@ -0,0 +1,5 @@
+export class Usuario {
+    nombre:string|undefined;
+    clave:string|undefined;
+
+}
